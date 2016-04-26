5640f3836710d63ec68e21c2e6fb18a150671575
uivlis <vladsilviufarcas@gmail.com>
2016-04-26 17:41:40 +0300

    HW 4 - first version Please tell me your opinion. I will add the documentation soon.

--- Farcas S/HW4/src/controllers/Main.java
+++ Farcas S/HW4/src/controllers/Main.java
@@ -5,25 +5,6 @@ import views.*;
 
 public class Main {
 	public static void main(String[] args) {
-//		Bank b = Bank.deserialize();
-//		Person p = new Person("Vlad", 21);
-//		Account a = null;
-//		try {
-//			a = new SavingsAccount(400);
-//		} catch (NotEnoughFundsException e1) {
-//			e1.printStackTrace();
-//		}
-//		a.addObserver(b);
-//		try {
-//			b.addPerson(p);
-//			b.addAccount(p, a);
-//		} catch (IllegalOperationException e) {
-//			e.printStackTrace();
-//		}
-//		a.deposit(200);
-//		b.serialize();
-//		System.out.println(b);
-//		System.out.println(b.readAccountInfo(a));
 		Bank.deserialize();
 		new LogInController(new LogIn("Bank"), false);
 	}
